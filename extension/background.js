ffbe672246700bcc481f16ba870ff76444136d5e
patrick bendorf <bendorf@nibis.de>
2015-09-10 11:35:50 +0200

    + added rewrite functionality to resource-urls like javascript

--- extension/background.js
+++ extension/background.js
@@ -10,11 +10,30 @@
 			if (details.url.indexOf(mapping.sourceUrl) == 0) {
 				var newUrl = mapping.destinationUrl + details.url.slice(mapping.sourceUrl.length);
 				// console.log('rewriting', details.url, 'to', newUrl);
-				
+
 				chrome.tabs.update(details.tabId, { url: newUrl })
 				break;
 			}
 		}
 	});
 
+	chrome.webRequest.onBeforeRequest.addListener(
+		function(details) {
+			var mappingKey = 'nl.sjmulder.urlrewrite.mappings';
+			var mappings = JSON.parse(localStorage[mappingKey] || '[]');
+
+			for (var i = 0; i < mappings.length; i++) {
+				var mapping = mappings[i];
+				if (details.url.indexOf(mapping.sourceUrl) == 0) {
+					var newUrl = mapping.destinationUrl + details.url.slice(mapping.sourceUrl.length);
+					// console.log('rewriting', details.url, 'to', newUrl);
+
+					return { redirectUrl: newUrl };
+					break;
+				}
+			}
+		},
+		{urls: ["*://*/*"]},
+		["blocking"]
+	);
 })();
